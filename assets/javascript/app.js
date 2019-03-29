f105a7c07c0fefd32bda9a7231825894f5c5af3b
christopher bell <christopherbell@christophersAir.fios-router.home>
2019-03-28 23:30:44 -0400

    MVP push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,6 @@
 // ### Instructions
 
-  // * Make sure that your app suits this basic spec:
-  //   * When adding trains, administrators should be able to submit the following:
-  //     * Train Name
-  //     * Destination 
-  //     * First Train Time -- in military time
-  //     * Frequency -- in minutes
   //   * Code this app to calculate when the next train will arrive; this should be relative to the current time.
-  //   * Users from many different machines must be able to view same train times.
-  //   * Styling and theme are completely up to you. Get Creative!
 
 // ### Bonus (Extra Challenges)
   // * Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
@@ -33,10 +25,10 @@ var config =  {
     var name = "";
     var destination = "";
     var time = "";
-    var frequency = "";
+    var frequency = "";        
 
     // Click Button changes what is stored in firebase
-    $("#click-button").on("click", function(event) {
+      $("#click-button").on("click", function(event) {
       // Prevent the page from refreshing
       event.preventDefault();
 
@@ -45,7 +37,6 @@ var config =  {
       destination = $("#dest-input").val().trim();
       time = $("#time-input").val().trim();
       frequency = $("#freq-input").val().trim();
-
 
       // Change what is saved in firebase
       database.ref().set({
@@ -59,27 +50,42 @@ var config =  {
     // Firebase 
     // When changes occurs it will print them to console and html
     database.ref().on("value", function(snapshot) {
+      var nextTrain = getMinutesTilNextTrain(snapshot.val().time, snapshot.val().frequency);
 
-      // Print the initial data to the console.
-      console.log(snapshot.val());
-
-      // Log the value of the various properties
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().destination);
-      console.log(snapshot.val().time);
-      console.log(snapshot.val().frequency);
-
-      // Add to the HTML table
-      $("#name-display").text(snapshot.val().name);
-      $("#dest-display").text(snapshot.val().destination);
-      $("#time-display").text(snapshot.val().time);
-      $("#freq-display").text(snapshot.val().frequency);
-
-
-
-    //   $("#displayed-data").text(snapshot.val().name + " | " + snapshot.val().destination + " | " + snapshot.val().time + " | " + snapshot.val().frequency);
-
-      // If any errors are experienced, log them to console.
-    }, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
+      // append to our table of trains, inside tbody, with a new row of the train data
+      $("#train-display").append(
+      "<tr><th>" + snapshot.val().name + "</th>" +
+      "<td>" + snapshot.val().destination + "</td>" +
+      "<td>" + snapshot.val().time + "</td>" +
+      "<td>" + snapshot.val().frequency + "</td>" +
+      "<td>" + nextTrain + "</td>"
+      );
+      
+      // Clear the textbox when done
+      $(".form-control").val("");
     });
+
+    function getMinutesTilNextTrain(firstTrain, frequency) {
+      var firstTrainOfTheDay = moment(firstTrain, 'HH:mm');
+  
+      // Get the current time
+      var currentTime = moment();
+  
+      // Create a variable to store the next train time
+      var minutesTilNextTrain = 0;
+  
+      // If the first time is in the future, set next train to then
+      if (firstTrainOfTheDay > currentTime) {
+        minutesTilNextTrain = firstTrainOfTheDay.diff(currentTime, 'minutes');
+      } else {
+        // Otherwise, get minutes past first time
+        var minutesPast = currentTime.diff(firstTrainOfTheDay, 'minutes');
+        // Find the result of minutesPast % frequency
+        var remainder = minutesPast % frequency;
+        // Subtract the remainder from the frequency
+        minutesTilNextTrain = frequency - remainder;
+      }
+
+      return minutesTilNextTrain;
+    }
+    
